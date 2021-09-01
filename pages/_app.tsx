ed946975f86d559a7c2ef0249534a95e467030a8
Andrei Tretyakov <andrei.tretyakov@gmail.com>
2021-09-01 18:14:48 +0300

    Setup tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
